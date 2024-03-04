ee9ab80b2d5b450a5fcaf2eb5048302dea751340
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2024-03-04 15:27:36 +0400

    added bgChanger project

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("pink")
+
+  return (
+    <div className='w-full h-screen duration-200' style={{ background: color }}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button
+            onClick={() => setColor("purple")}
+            className='outline-none px-4 py-1 rounded-full shadow-lg text-black'>purple</button>
+          <button
+            onClick={() => setColor("grey")}
+            className='outline-none px-4 py-1 rounded-full shadow-lg text-black'>grey</button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
